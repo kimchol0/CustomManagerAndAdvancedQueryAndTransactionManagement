7d117652b44be382c9454331557f97775ec8cb1b
KimChol <kimchol2ece@outlook.com>
2020-10-25 17:58:41 +0800

    创建模型类并生成迁移文件

--- stu/models.py
+++ stu/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
+
 # Create your models here.
+class Clazz(models.Model):
+    cname = models.CharField(max_length=30)
+
+
+class Student(models.Model):
+    sname = models.CharField(max_length=30)
+    score = models.PositiveIntegerField()
+    cls = models.ForeignKey(Clazz,on_delete=models.CASCADE)
